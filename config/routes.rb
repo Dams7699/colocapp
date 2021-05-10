e68cef9bc0594996b1c7eaf80ab8e6ff3696b585
Axel Ridray <axel.ridray@sfr.fr>
2021-05-10 22:47:50 +0200

    je crois que c'est bon j'ai un doute

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,17 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :colocations do
+  resources :offers, only: [ :new, :create ]
+  end
+
+  resources :offers do
+    member { patch :accept }
+    member { patch :decline }
+    member { patch :cancel }
+  end
+
+  resource :profil, only: [ :show ]
+  resource :notification, only: [ :show ]
+  end
 end
--- db/migrate/20210510190006_add_user_to_colocation.rb
+++ db/migrate/20210510190006_add_user_to_colocation.rb
@@ -1,5 +1,13 @@
 class AddUserToColocation < ActiveRecord::Migration[6.0]
   def change
-    remove_reference :colocations, :user, null: false, foreign_key: true
+      create_table :colocations do |t|
+      t.string :address
+      t.text :description
+      t.integer :price
+      t.integer :desired_people
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
   end
 end
