bbfe5ddb10193de889bbb36ce957d08429485615
Damien Menages <menages.damien@gmail.com>
2021-05-12 12:00:22 +0200

    creationshow

--- config/routes.rb
+++ config/routes.rb
@@ -3,14 +3,14 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :colocations do
-  resources :offers, only: [ :new, :create ]
+  # => resources :offers, only: [ :new, :create ]
   end
 
-  resources :offers do
-    member { patch :accept }
-    member { patch :decline }
-    member { patch :cancel }
-  end
+# =>   resources :offers do
+ # =>    member { patch :accept }
+ # =>    member { patch :decline }
+# =>  member { patch :cancel }
+  # => end
 
   resource :profil, only: [ :show ]
   resource :notification, only: [ :show ]
