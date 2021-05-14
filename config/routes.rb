5e44b8eab54522faf4b85e6f39c76b3e1c370527
Teyant Alexandre qextzy@gmail.com <qextzy@gmail.com>
2021-05-14 17:11:55 +0200

    debug

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :colocations do
-  # => resources :offers, only: [ :new, :create ]
+  resources :offers, only: [ :create ]
   end
 
 # =>   resources :offers do
