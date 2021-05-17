29381ec62c4e36b0e0384173dcaf27168a1c7a96
Axel Ridray <axel.ridray@sfr.fr>
2021-05-17 13:07:40 +0200

    add photo et correction js

---
+++ app/javascript/custom/image_upload.js
@@ -0,0 +1,20 @@
+const imageUpload = () => {
+  const csrfToken = document.querySelector("meta[name=csrf-token]").content
+  const input = document.getElementById("user_photo")
+  if (input) {
+    input.addEventListener("change", (e) => {
+      const file = input.files[0]
+      let formData = new FormData()
+      formData.append('user[photo]', file)
+      formData.append('authenticity_token', csrfToken)
+
+      fetch("/profil", 
+        {
+          method: "put",
+          body: formData
+        }
+      ).then(response => { document.location.reload() })
+    });
+  }
+};
+export { imageUpload }
