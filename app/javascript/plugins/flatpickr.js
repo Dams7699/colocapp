052d06db93f255e1ba36a0b9f000235a39875daf
Teyant Alexandre qextzy@gmail.com <qextzy@gmail.com>
2021-05-20 12:08:55 +0200

    adding limite age

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,7 +1,9 @@
 import flatpickr from "flatpickr";
 
 const initFlatpickr = () => {
-  flatpickr(".datepicker", {});
+  flatpickr(".datepicker", {
+    minDate: "2003-01"
+  });
 }
 
 export { initFlatpickr };
